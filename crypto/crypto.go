162674bd58bdedb16f1950b34f9123c33331f70c
Tijmen van der Burgt <tvdburgt@gmail.com>
2013-12-21 19:18:05 +0100

    favor panic over error for some crypto functions

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -15,52 +15,45 @@ const keySize = 32       // 256-bit key for AES-256
 var macHash = sha256.New // Use SHA-256 as HMAC
 
 // Generates a cryptographically secure salt with length equal to key size.
-func GenerateRandomSalt() ([]byte, error) {
-	salt := make([]byte, keySize)
-	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
-		return nil, err
+func GenerateRandomSalt() (salt []byte, err error) {
+	salt = make([]byte, keySize)
+	if _, err = io.ReadFull(rand.Reader, salt); err != nil {
+		return
 	}
-	return salt, nil
-}
-
-func deriveKeys(passphrase, salt []byte) (cipherKey, hmacKey []byte, err error) {
-	// Use sensible work factor defaults for now...
-	key, err := scrypt.Key(passphrase, salt, 16384, 8, 1, 64) // keySize*2
-
-	if err == nil {
-		cipherKey, hmacKey = key[:keySize], key[keySize:]
-	}
-
 	return
 }
 
-func initCTRStream(key []byte) (cipher.Stream, error) {
-	block, err := aes.NewCipher(key)
+func deriveKeys(passphrase, salt []byte) (cipherKey, hmacKey []byte) {
+	// Use sensible work factor defaults for now...
+	key, err := scrypt.Key(passphrase, salt, 16384, 8, 1, 64) // keySize*2
 	if err != nil {
-		return nil, err
+		panic(err) // scrypt params should be guarded elsewhere
 	}
-	iv := make([]byte, block.BlockSize()) // IV is always {0, 0, ...}
-	return cipher.NewCTR(block, iv), nil
+	if err == nil {
+		cipherKey, hmacKey = key[:keySize], key[keySize:]
+	}
+	return
 }
 
-func InitStreamParams(passphrase, salt []byte) (stream cipher.Stream, mac hash.Hash, err error) {
-	cipherKey, hmacKey, err := deriveKeys(passphrase, salt)
+func initCTRStream(key []byte) cipher.Stream {
+	block, err := aes.NewCipher(key)
+	// TODO: internal state of block remains in memory?
 	if err != nil {
-		return
+		panic(err) // KeySizeError
 	}
+	iv := make([]byte, block.BlockSize()) // IV is always {0, 0, ...}
+	return cipher.NewCTR(block, iv)
+}
+
+func InitStreamParams(passphrase, salt []byte) (stream cipher.Stream, mac hash.Hash) {
+	cipherKey, hmacKey := deriveKeys(passphrase, salt)
 	defer Clear(cipherKey, hmacKey)
-
-	if stream, err = initCTRStream(cipherKey); err != nil {
-		return
-	}
-
+	stream = initCTRStream(cipherKey)
 	mac = hmac.New(macHash, hmacKey)
 	return
 }
 
 // Clears sensitive data from memory (useful for plaintext passwords etc.)
-// https://groups.google.com/forum/?fromgroups=#!topic/golang-nuts/sKQtvluD_So
-// https://groups.google.com/forum/#!msg/golang-nuts/KvgjNbCXTY4/uigWOtc6bJcJ
 func Clear(s ...[]byte) {
 	for _, secret := range s {
 		for i := range secret {
