f9b4f846c655d99c589783a4dbaf33209f3b7c3d
Tijmen van der Burgt <tvdburgt@gmail.com>
2013-12-25 23:19:19 +0100

    finish rudimentary version of 'passman clip' command (linux only)

--- clip.go
+++ clip.go
@@ -1,76 +1,80 @@
 package main
 
 import (
-	_ "errors"
-	_ "flag"
 	"fmt"
-	_ "github.com/atotto/clipboard"
-	_ "os"
 	"github.com/tvdburgt/passman/clipboard"
 	"time"
 )
 
 var cmdClip = &Command{
-	Run: runClip,
 	UsageLine: "clip identifier",
-	Short: "display an individual entry",
+	Short:     "display an individual entry",
 	Long: `
 get displays the content of a single passman entry. The identifier must be an
 exact match. To search or display multiple entries, see passman list.
 	`,
 }
 
+const clipTimeoutDefault = 20 * time.Second
+
+var (
+	clipTimeout time.Duration
+	clipPersist bool
+	// TODO: verbose?
+)
+
+func init() {
+	cmdClip.Run = runClip
+	cmdClip.Flag.DurationVar(&clipTimeout, "timeout", clipTimeoutDefault, "")
+	cmdClip.Flag.BoolVar(&clipPersist, "persist", false, "")
+}
+
 func runClip(cmd *Command, args []string) {
-	// if len(args) < 1 {
-	// 	fatalf("passman clip: missing identifier")
-	// }
-	// id := args[0]
+	if len(args) < 1 {
+		fatalf("passman clip: missing identifier")
+	}
+	id := args[0]
+
+	if clipTimeout <= 0 {
+		fatalf("passman clip: negative timeout")
+	}
+
+	s, err := readPassStore();
+	if err != nil {
+		fatalf("passman clip: %s", err)
+	}
+	e, ok := s.Entries[id]
+	if !ok {
+		fatalf("passman get: no such entry '%s'", id)
+	}
 
 	if err := clipboard.Setup(); err != nil {
 		fatalf("passman clip: %s", err)
 	}
-
-	// TODO: run event loop in goroutine
-	// TODO: add verbose flag
-	err := clipboard.Put([]byte("examplepassw0rd"));
+	creq, cerr, err := clipboard.Put(e.Password)
 	if err != nil {
 		fatalf("passman clip: %s", err)
 	}
 
-	time.Sleep(5 * time.Second)
+	fmt.Println("Listening for clipboard requests...")
 
-	fmt.Println("Done sleeping; exiting...")
-
-	return
-
-
-	// var store *Store
-	// var entry *Entry
-	// var ok bool
-
-	// // fs := flag.NewFlagSet(os.Args[1], flag.ExitOnError)
-	// // fs.Parse(os.Args[2:])
-	// if len(os.Args) < 3 {
-	// 	return errors.New("missing id argument")
-	// }
-	// id := os.Args[2]
-
-	// passphrase := readPass("Enter passphrase for '%s'", storePath)
-	// defer clear(passphrase)
-	// if store, err = openStore(passphrase); err != nil {
-	// 	return
-	// }
-
-	// if entry, ok = store.Entries[id]; !ok {
-	// 	return fmt.Errorf("no such entry '%s'", id)
-	// }
-
-	// if err = clipboard.WriteAll(entry.Password); err != nil {
-	// 	return
-	// }
-
-	// fmt.Println("Copied password to clipboard")
-	// fmt.Println(entry.Password)
-
-	// return
+	for {
+		select {
+		case req := <-creq:
+			fmt.Printf("Password requested by '%s'\n", req)
+			if !clipPersist {
+				fmt.Println("Exiting... Add '-persist' flag to allow additional requests.")
+				return
+			}
+		case err := <-cerr:
+			fatalf("passman clip: %s", err)
+		case <-time.After(clipTimeout):
+			fmt.Printf("Reached timeout (%s), exiting.", clipTimeout)
+			if clipTimeout == clipTimeoutDefault {
+				fmt.Print(" Duration can be changed with the '-timeout' flag.")
+			}
+			fmt.Println()
+			return
+		}
+	}
 }
--- clipboard/clipboard_linux.go
+++ clipboard/clipboard_linux.go
@@ -1,25 +1,65 @@
-// API for data exchange with X11 clipboard (PRIMARY and CLIPBOARD)
-// Global documentation: http://www.jwz.org/doc/x-cut-and-paste.html
-// Technical documentation: http://tronche.com/gui/x/icccm/sec-2.html
+// Package clipboard provides a simple API for handling data exchange between
+// the Go client and system clipboard.
+// This package currently only supports small data strings for PRIMARY and
+// CLIPBOARD selections.
+//
+// Useful things to read:
+// Intro to X selections: http://www.jwz.org/doc/x-cut-and-paste.html
+// Technical ICCCM manual: http://www.x.org/releases/X11R7.6/doc/xorg-docs/specs/ICCCM/icccm.html
 package clipboard
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"github.com/BurntSushi/xgb"
 	"github.com/BurntSushi/xgb/xproto"
+	"github.com/BurntSushi/xgbutil"
+	"github.com/BurntSushi/xgbutil/icccm"
+	// "log"
 )
 
 var (
-	conn  *xgb.Conn
-	win   xproto.Window
-	setup *xproto.SetupInfo
+	conn     *xgb.Conn
+	connUtil *xgbutil.XUtil
+	win      xproto.Window
+	setup    *xproto.SetupInfo
+
+	atomClipboard xproto.Atom
+	atomTargets   xproto.Atom
+	atomUtf8      xproto.Atom
+	atomMultiple  xproto.Atom
+	atomTimestamp xproto.Atom
+
+	atoms = map[string]*xproto.Atom{
+		"CLIPBOARD":   &atomClipboard,
+		"TARGETS":     &atomTargets,
+		"UTF8_STRING": &atomUtf8,
+		"MULTIPLE":    &atomMultiple,
+		"TIMESTAMP":   &atomTimestamp,
+	}
+
+	selectionTime xproto.Timestamp
 )
+
+// var Debug debug
+
+// type debug bool
+
+// func (d debug) Printf(format string, args ...interface{}) {
+// 	if d {
+// 		log.Printf(format, args...)
+// 	}
+// }
 
 // Performs initialization for clipboard to work. Not using init() to prevent
 // unnecessary invocations.
 func Setup() (err error) {
 	if conn, err = xgb.NewConn(); err != nil {
+		return
+	}
+	if connUtil, err = xgbutil.NewConn(); err != nil {
 		return
 	}
 
@@ -31,9 +71,12 @@ func Setup() (err error) {
 		return
 	}
 
+	if err = initAtoms(); err != nil {
+		return
+	}
+
 	// Create dummy window for capturing Selection events
-	xproto.CreateWindow(conn, 0, win, screen.Root, 0, 0, 1, 1,
-		0,
+	xproto.CreateWindow(conn, 0, win, screen.Root, 0, 0, 1, 1, 0,
 		xproto.WindowClassCopyFromParent, screen.RootVisual,
 		xproto.CwEventMask,
 		[]uint32{xproto.EventMaskPropertyChange})
@@ -41,35 +84,66 @@ func Setup() (err error) {
 	return
 }
 
-func Put(data []byte) (err error) {
+// Initializes all required atom identifiers that are used in this package
+func initAtoms() (err error) {
+	for name, atom := range atoms {
+		atomCookie := xproto.InternAtom(conn, true, uint16(len(name)), name)
+		atomReply, err := atomCookie.Reply()
+		if err != nil {
+			return err
+		}
+		*atom = atomReply.Atom
+	}
+	return
+}
+
+// Makes data available for PRIMARY and CLIPBOARD selection requests. This
+// function does not block. Requestor names are sent to the string channel and
+// errors are sent to the error channel.
+func Put(data []byte) (<-chan string, <-chan error, error) {
+	creq := make(chan string)
+	cerr := make(chan error)
+
 	if conn == nil {
-		return errors.New("no X connection, did you run clipboard.Setup() beforehand?")
+		return nil, nil, errors.New("clipboard: X connection is nil")
 	}
 
-	fmt.Println("timestamps:")
-	fmt.Println(xproto.Timestamp(0))
-	fmt.Println(xproto.TimeCurrentTime)
+	var err error
+	selectionTime, err = getCurrentTime()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// TODO: close chan on error?
+	go put(data, xproto.AtomPrimary, creq, cerr)
+	go put(data, atomClipboard, creq, cerr)
+
+	return creq, cerr, nil
+}
+
+func put(data []byte, selection xproto.Atom,
+	creq chan<- string, cerr chan<- error) {
 
 	// Assert selection ownership
-	var selection xproto.Atom = xproto.AtomPrimary
-	time, err := getCurrentTime()
-	if err != nil {
-		return err
-	}
-	// time is 0
-	xproto.SetSelectionOwner(conn, win, selection, time)
+	xproto.SetSelectionOwner(conn, win, selection, selectionTime)
 
 	// Check if selection owner has changed to our window
 	cookie := xproto.GetSelectionOwner(conn, selection)
-	reply, _ := cookie.Reply()
-	// TODO: change panic to err
+	reply, err := cookie.Reply()
+	if err != nil {
+		cerr <- err
+		return
+	}
 	if reply.Owner != win {
-		panic("unable to become selection owner")
+		cerr <- errors.New("clipboard: unable to become selection owner")
+		return
 	}
 
 	// Check if data exceeds maximum length
 	if len(data) > int(setup.MaximumRequestLength) {
-		return errors.New("selection data is too large") // add size
+		cerr <- fmt.Errorf("clipboard: selection data length (%d) "+
+			"exceeds maximum request length (%d)",
+			len(data), setup.MaximumRequestLength)
 	}
 
 	// Start the main event loop.
@@ -81,95 +155,142 @@ func Put(data []byte) (err error) {
 		// An error can only be seen here as a response to an unchecked
 		// request.
 		var e xgb.Event
-		e, err = conn.WaitForEvent()
-		if e == nil && err == nil {
-			fmt.Println("Both event and error are nil. Exiting...")
-			// TODO: return err
+		if e, err = conn.WaitForEvent(); err != nil {
+			return // Error can be ignored?
+		}
+		if e == nil {
+			panic("both event and error are nil")
+		}
+
+		switch e := e.(type) {
+		case xproto.SelectionRequestEvent:
+			handleRequest(e, data, creq)
+
+		case xproto.SelectionClearEvent:
+			fmt.Println("lost selection", e)
 			return
+
+		default:
+			fmt.Println("unknown event:", e)
 		}
-
-		// type SelectionRequestEvent struct {
-		//     Sequence uint16
-		//     // padding: 1 bytes
-		//     Time      Timestamp
-		//     Owner     Window
-		//     Requestor Window
-		//     Selection Atom
-		//     Target    Atom
-		//     Property  Atom
-		// }
-		req, ok := e.(xproto.SelectionRequestEvent)
-		if !ok {
-			continue
-		}
-		if err != nil {
-			panic(err)
-		}
-
-		// type SelectionNotifyEvent struct {
-		//     Sequence uint16
-		//     // padding: 1 bytes
-		//     Time      Timestamp
-		//     Requestor Window
-		//     Selection Atom
-		//     Target    Atom
-		//     Property  Atom
-		// }
-		res := xproto.SelectionNotifyEvent{
-			// sequence?
-			Time:      req.Time,
-			Requestor: req.Requestor,
-			Selection: req.Selection,
-			Target:    req.Target,
-		}
-
-		// Check for obsolete requestor
-		if req.Property == xproto.AtomNone {
-			res.Property = req.Target
-		} else {
-			res.Property = req.Property
-		}
-
-		fmt.Println(res)
-
-		// if req.Target == xproto.AtomString {
-
-		xproto.ChangeProperty(conn, xproto.PropModeReplace,
-			res.Requestor, res.Property, xproto.AtomString,
-			8,
-			uint32(len(data)), data)
-
-		fmt.Println(res.Bytes())
-		xproto.SendEvent(conn, false, res.Requestor,
-			xproto.EventMaskNoEvent,
-			string(res.Bytes()))
-
-		// }
-
-		// check timestamp window => prop None
-
-		// property
-
-		fmt.Println(req)
 	}
 }
 
+func handleRequest(req xproto.SelectionRequestEvent, data []byte,
+	creq chan<- string) {
+
+	// Build response
+	res := xproto.SelectionNotifyEvent{
+		Requestor: req.Requestor,
+		Selection: req.Selection,
+		Target:    req.Target,
+		Property:  req.Property,
+		Time:      req.Time,
+	}
+
+	// If requestor is obsolete, set target as property atom
+	if res.Property == xproto.AtomNone {
+		fmt.Println("obsolete requestor")
+		res.Property = res.Target
+	}
+
+	// Refuse request if request timestamp is earlier than time of initial
+	// ownership
+	var converted bool
+	if req.Time != xproto.TimeCurrentTime && req.Time < selectionTime {
+		req.Property = xproto.AtomNone
+		fmt.Println("wrong timestamp")
+	} else {
+		converted = handleResponse(res, data)
+	}
+
+	// Notify requestor by responding with a SelectNotify event
+	xproto.SendEvent(conn, false, res.Requestor,
+		xproto.EventMaskNoEvent,
+		string(res.Bytes()))
+
+	if converted {
+		name, _ := icccm.WmNameGet(connUtil, req.Requestor)
+		// Run in goroutine so it doesn't block if creq is full
+		go func() { creq <- name }()
+	}
+}
+
+// Sends response to selection request by changing a designated property on
+// requestor's window. A boolean is returned, indicating whether selection data
+// is sent.
+func handleResponse(res xproto.SelectionNotifyEvent, data []byte) bool {
+	switch res.Target {
+	// TARGETS: send list of supported target atoms
+	// Some apps send multiple identical TARGETS requests and then revert
+	// to UTF8_STRING/STRING, so this might not be functioning correctly
+	case atomTargets:
+		// fmt.Println("TARGETS:", res)
+		buf := new(bytes.Buffer)
+		// Supported target atoms
+		targets := []xproto.Atom{
+			xproto.AtomString,
+			atomUtf8,
+			atomTargets,
+			atomTimestamp,
+		}
+		for _, atom := range targets {
+			binary.Write(buf, binary.LittleEndian, uint32(atom))
+		}
+		xproto.ChangeProperty(conn, xproto.PropModeReplace,
+			res.Requestor, res.Property, xproto.AtomAtom,
+			32, uint32(len(targets)), buf.Bytes())
+
+	// TODO: MULTIPLE target case required?
+
+	// TIMESTAMP: return timestamp used to acquire selection ownership
+	case atomTimestamp:
+		// fmt.Println("TIMESTAMP:", res)
+		timestamp := make([]byte, 4)
+		xgb.Put32(timestamp, uint32(selectionTime))
+		xproto.ChangeProperty(conn, xproto.PropModeReplace,
+			res.Requestor, res.Property, xproto.AtomInteger,
+			32, 1, timestamp)
+
+	// STRING, UTF8_STRING: send selection data
+	case xproto.AtomString, atomUtf8:
+		// fmt.Println("STRING:", res)
+		xproto.ChangeProperty(conn, xproto.PropModeReplace,
+			res.Requestor, res.Property, res.Target,
+			8, uint32(len(data)), data)
+
+		return true
+
+		// TODO: watch PropertyNotify event on requestor window to
+		// verify data is transferred successfully (property must be deleted)
+
+	// Unknown atom target: refuse request by setting Property to None
+	default:
+		// fmt.Println("unknown target:", res)
+		res.Property = xproto.AtomNone
+	}
+
+	return false
+}
+
+// Returns time of X server
 func getCurrentTime() (time xproto.Timestamp, err error) {
 	// Perfrom a zero-length append to a property of our dummy window
 	xproto.ChangeProperty(conn, xproto.PropModeAppend, win, xproto.AtomWmName,
 		xproto.AtomString, 8, 0, []byte{})
 
-	// Retrieve X' timestamp from PropertyNotify event
+	// Retrieve timestamp from PropertyNotify event
 	for {
 		var e xgb.Event
 		if e, err = conn.WaitForEvent(); err != nil {
 			return
 		}
-
+		if e == nil {
+			panic("both event and error are nil")
+		}
 		if e, ok := e.(xproto.PropertyNotifyEvent); ok {
 			return e.Time, nil
 		}
 	}
-
 	return
 }
