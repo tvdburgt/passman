fe683a238d4981a26fdbeff0f7633ad2e9881ca2
Tijmen van der Burgt <tvdburgt@gmail.com>
2013-12-27 23:11:31 +0100

    extend clip command to optionally use name and metadata fields

--- clip.go
+++ clip.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/tvdburgt/passman/clipboard"
+	"github.com/tvdburgt/passman/store"
+	"strings"
 	"time"
 )
 
@@ -18,15 +20,17 @@ exact match. To search or display multiple entries, see passman list.
 const clipTimeoutDefault = 20 * time.Second
 
 var (
-	clipTimeout time.Duration
-	clipPersist bool
-	// TODO: verbose?
+	clipTimeout = clipTimeoutDefault
+	clipPersist = false
+	clipValue   = "password"
+	// TODO: -verbose?
 )
 
 func init() {
 	cmdClip.Run = runClip
-	cmdClip.Flag.DurationVar(&clipTimeout, "timeout", clipTimeoutDefault, "")
-	cmdClip.Flag.BoolVar(&clipPersist, "persist", false, "")
+	cmdClip.Flag.DurationVar(&clipTimeout, "timeout", clipTimeout, "")
+	cmdClip.Flag.BoolVar(&clipPersist, "persist", clipPersist, "")
+	cmdClip.Flag.StringVar(&clipValue, "value", clipValue, "")
 }
 
 func runClip(cmd *Command, args []string) {
@@ -36,10 +40,10 @@ func runClip(cmd *Command, args []string) {
 	id := args[0]
 
 	if clipTimeout <= 0 {
-		fatalf("passman clip: negative timeout")
+		fatalf("passman clip: nonpositive timeout")
 	}
 
-	s, err := readPassStore();
+	s, err := readPassStore()
 	if err != nil {
 		fatalf("passman clip: %s", err)
 	}
@@ -48,10 +52,12 @@ func runClip(cmd *Command, args []string) {
 		fatalf("passman get: no such entry '%s'", id)
 	}
 
+	value := getValue(e)
+
 	if err := clipboard.Setup(); err != nil {
 		fatalf("passman clip: %s", err)
 	}
-	creq, cerr, err := clipboard.Put(e.Password)
+	creq, cerr, err := clipboard.Put(value)
 	if err != nil {
 		fatalf("passman clip: %s", err)
 	}
@@ -77,4 +83,31 @@ func runClip(cmd *Command, args []string) {
 			return
 		}
 	}
+}
+
+func validValues(e *store.Entry) (values []string) {
+	values = make([]string, 2, len(e.Metadata)+2)
+	values[0] = "'password'"
+	values[1] = "'name'"
+	for key, _ := range e.Metadata {
+		values = append(values, fmt.Sprintf("'%s'", key))
+	}
+	return
+}
+
+func getValue(e *store.Entry) []byte {
+	switch clipValue {
+	case "password":
+		return e.Password
+	case "name":
+		return []byte(e.Name)
+	default:
+		if value, ok := e.Metadata[clipValue]; ok {
+			return []byte(value)
+		}
+		// TODO: fatalf here or bubble error to runClip?
+		fatalf("passman clip: invalid value '%s' (valid values: %s)",
+			clipValue, strings.Join(validValues(e), ", "))
+	}
+	return nil
 }
